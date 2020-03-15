b93f42967aaead4c7cb0bdff7e540e01c86a232d
xuzhiming <2696700220@qq.com>
2020-03-15 14:06:55 +0800

    first commit

---
+++ config/ind_db.php
@@ -0,0 +1,10 @@
+<?php
+defined('IN_IA') or define('IN_IA', true);
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;port=3306;dbname=maiye_chushengwa',
+    'username' => 'maiye_chushengwa',
+    'password' => 'mYSWZeac6r7NGeh5',
+    'charset' => 'utf8',
+    'tablePrefix' => 'hjmallind_',
+];
